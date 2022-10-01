7017d3fc418c4b43a3debaa85f09cad6e7d3b6be
Blas Santomé Ocampo <bsantome@mercadona.es>
2022-10-01 19:18:11 +0200

    feat(dark-side): Add antialias

--- src/app/services/renderer.service.ts
+++ src/app/services/renderer.service.ts
@@ -12,7 +12,7 @@ export class RendererService {
   #renderer: WebGLRenderer;
 
   constructor() {
-    this.#renderer = new WebGLRenderer();
+    this.#renderer = new WebGLRenderer({antialias: true});
     this.#camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 900000000);
     this.#controls = new OrbitControls(this.#camera, this.#renderer.domElement);
     this.#initialize();
